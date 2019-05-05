c475579c9245228d50422e5b7beb239d7fb079b9
gwonii <hc.gwonii@gmail.com>
2019-05-05 07:39:26 +0000

    2019/05/05

---
+++ CodingTest/Code/test.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  
+	int a,b;
+	cin>>a>>b;
+	cout<<a<<b;
+
+  scanf("%d %d", &a,&b);
+	printf("%d %d", a,b);
+
+}
+
+
+
---
+++ CodingTest/Code/test5.c
@@ -0,0 +1,18 @@
+
+//#include <iostream>
+#include <stdio.h>
+
+
+//using namespace std;
+
+int main() {
+
+  int a;
+
+	int b,c,d;
+
+	a = scanf("%d %d %d", &b,&c,&d );
+
+  printf("%d", a);
+
+}
